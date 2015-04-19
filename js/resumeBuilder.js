3c5cd0d9f5958c20cf654f053297b1ca17246e37
romozart <roman.bulldigaetano@gordon.edu>
2015-04-19 18:45:57 -0400

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,10 @@
-var formattedName = HTMLheaderName.replace("%data%", "Roman Bull Di Gaetano");
-var formattedRole = HTMLheaderRole.replace("%data%", "programmer");
+var me = {
+  "name": "Roman Bull Di Gaetano",
+  "role": "programming"
+};
+
+var formattedName = HTMLheaderName.replace("%data%", me.name);
+var formattedRole = HTMLheaderRole.replace("%data%", me.role);
 
 var work = {
   "title": "programmer",
@@ -12,5 +17,5 @@ var workDsc = HTMLworkDescription.replace("%data%", work.description);
 $("#main").append(formattedName);
 $("#main").append(formattedRole);
 
-$("#main").append(workTitle);
-$("#main").append(workDsc);
+$("#workExperience").append(workTitle);
+$("#workExperience").append(workDsc);
